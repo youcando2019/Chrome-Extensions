35afc9116e9a17abe0a4eb18c991d9d9110b4c2b
You Can Do <youcando2019@gmail.com>
2019-07-13 21:32:44 +0100

    Initial commit

---
+++ cats/content.js
@@ -0,0 +1,6 @@
+var images = document.getElementsByTagName("img");
+for (var i = images.length-1; i >=0; i--)
+{
+  images[i].src = "https://pixel.nymag.com/imgs/fashion/daily/2019/03/15/15-cat-bathtub.w700.h467.jpg"
+  //images[i].parentNode.removeChild(images[i]);
+}
